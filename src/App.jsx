bc92d27f2de6fffc7cc67f91a10a553b53aa477c
Ahmudul <64348386+AhmudulHassan@users.noreply.github.com>
2024-05-15 19:53:34 +0600

    right side button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
 
-import Test from "./components/Test/Test";
+
 // import Navbar from "./components/Navbar";
 // import Navbar from "./components/Navbar";
 
